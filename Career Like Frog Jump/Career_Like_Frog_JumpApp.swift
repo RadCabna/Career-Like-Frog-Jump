ad86d8768f1c6a4dd115ece0bc6beba966dcb141
Radcabna <radcabna@gmail.com>
2026-06-01 13:31:10 +0300

    Initial Commit

---
+++ Career Like Frog Jump/Career_Like_Frog_JumpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Career_Like_Frog_JumpApp.swift
+//  Career Like Frog Jump
+//
+//  Created by Алкександр Степанов on 01.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct Career_Like_Frog_JumpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
